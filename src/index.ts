e2ab4811edfe51aff9296e5757d5ae304346d2b9
David Legendre <legendre2013@hotmail.com>
2022-10-20 13:47:44 -0500

    probando index

--- src/index.ts
+++ src/index.ts
@@ -2,7 +2,7 @@ import mongoConnect from "./global/config/DB/mongo";
 import app from './app'
 import localConfig from "./global/config/default";
 import log from './global/utils/logger';
-const port = localConfig().port
+const port = localConfig().port;
 app.listen(port, () => {
     log.info(`server on port http://localhost:${port}`)
     log.info('Connecting to DB, please wait')
