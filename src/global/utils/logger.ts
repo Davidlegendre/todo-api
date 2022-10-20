01ec08345e88fe76dafacb900d518ac59813fa13
David Legendre <legendre2013@hotmail.com>
2022-10-20 14:26:33 -0500

    ahora si

--- src/global/utils/logger.ts
+++ src/global/utils/logger.ts
@@ -1,7 +1,8 @@
 import dayjs from "dayjs";
 
-const log = (msn: string) => {
-    const date = `${dayjs().format()}`;
-    console.log(`[${date}] ` + msn)
-}
-export default log
+const log = (msn: string, isError: boolean) => {
+  const date = `${dayjs().format()}`;
+  if (!isError) console.log(`[${date}] ` + msn);
+  else console.error(`[${date}] ` + msn);
+};
+export default log;
