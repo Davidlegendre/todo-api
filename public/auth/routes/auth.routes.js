ea731ea12c7566df5e467600fe497396a9a25359
David Legendre <legendre2013@hotmail.com>
2022-10-20 13:46:31 -0500

    probando index

--- public/auth/routes/auth.routes.js
+++
@@ -1,8 +0,0 @@
-"use strict";
-Object.defineProperty(exports, "__esModule", { value: true });
-const express_1 = require("express");
-const router = (0, express_1.Router)();
-router.get("/hola", (req, res) => {
-    res.status(200).json({ "msg": "mundo" });
-});
-exports.default = router;
--- public/global/helpers/mensajeJson.js
+++
@@ -1,6 +0,0 @@
-"use strict";
-Object.defineProperty(exports, "__esModule", { value: true });
-function Mensaje(res, status, ok, data) {
-    return res.status(status).json({ status, ok, data });
-}
-exports.default = Mensaje;
