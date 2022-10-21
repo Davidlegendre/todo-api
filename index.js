23c806506802e9cad1cec46cc8be9064fa2fbb62
David Legendre <legendre2013@hotmail.com>
2022-10-20 19:25:23 -0500

    mod de ficheros

---
+++ index.js
@@ -0,0 +1,10 @@
+import mongoConnect from "./dist/global/config/DB/mongo";
+import app from './dist/app'
+import localConfig from "./dist/global/config/default";
+import log from './dist/global/utils/logger';
+const port = localConfig().port
+app.listen(port, () => {
+    log(`server on port http://localhost:${port}`, false)
+    log('Connecting to DB, please wait', false)
+    mongoConnect()
+})
