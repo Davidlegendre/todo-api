776acd45d2b4dba9363fa7431b55436e2691d406
David Legendre <legendre2013@hotmail.com>
2022-10-20 19:49:08 -0500

    mod de ficheros

--- index.js
+++ index.js
@@ -3,10 +3,10 @@ var __importDefault = (this && this.__importDefault) || function (mod) {
     return (mod && mod.__esModule) ? mod : { "default": mod };
 };
 Object.defineProperty(exports, "__esModule", { value: true });
-const mongo_1 = __importDefault(require("./global/config/DB/mongo"));
-const app_1 = __importDefault(require("./app"));
-const default_1 = __importDefault(require("./global/config/default"));
-const logger_1 = __importDefault(require("./global/utils/logger"));
+const mongo_1 = __importDefault(require("./public/global/config/DB/mongo"));
+const app_1 = __importDefault(require("./public/app"));
+const default_1 = __importDefault(require("./public/global/config/default"));
+const logger_1 = __importDefault(require("./public/global/utils/logger"));
 const port = (0, default_1.default)().port;
 app_1.default.listen(port, () => {
     (0, logger_1.default)(`server on port http://localhost:${port}`, false);
